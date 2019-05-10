35650c16396eb407927794eade919db99d98b8c5
Hansung Lee <hanlee@GOLD-POINT.COM>
2019-05-10 08:52:01 -0700

    add files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Fullscreen-Video-Website-NodeJs-Server
+# Fullscreen-Video-Website-Server-Side
+
+Fullscreen Video Website Server
+
+## Screenshot
+* Main Page
+<p align="center">
+  <img width="600" height="400" src="./assets/images/FullscreenWebsiteMainPage.PNG">
+</p>
---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+// playing speed of videos
+document.getElementById("myVideo").playbackRate = 0.5;
